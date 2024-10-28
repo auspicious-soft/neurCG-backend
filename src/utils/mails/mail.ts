12e0d5db66ab165ffb85fcebb03798b5a364e826
Auspicious <info@auspicioussoft.com>
2024-10-28 15:27:53 +0530

    Refactor sendPasswordResetEmail function to fix return statement

--- src/utils/mails/mail.ts
+++ src/utils/mails/mail.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-    await resend.emails.send({
+   return await resend.emails.send({
         from: process.env.COMPANY_RESEND_GMAIL_ACCOUNT as string,
         to: email,
         subject: "Reset your password",
