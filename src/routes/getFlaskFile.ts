c9d5d5c2e80251ce348f937e4a17808118dd5159
Auspicious <info@auspicioussoft.com>
2024-12-05 18:11:54 +0530

    Enhance getFlaskFile route to dynamically set content type based on response headers

--- src/routes/getFlaskFile.ts
+++ src/routes/getFlaskFile.ts
@@ -1,34 +1,37 @@
 import axios from "axios";
 import { configDotenv } from "dotenv";
 import { Request, Response, Router } from "express";
+
 const router = Router();
-configDotenv()
+configDotenv();
 
 router.post("/", async (req: Request, res: Response) => {
-    const { subpath } = req.body
-    const flaskUrl = process.env.FLASK_BACKEND_ML_URL as string
+    const { subpath } = req.body;
+    const flaskUrl = process.env.FLASK_BACKEND_ML_URL as string;
     try {
-        const formData = new FormData()
-        formData.append("subpath", subpath)
+        const formData = new FormData();
+        formData.append("subpath", subpath);
 
         const response = await axios.post(`${flaskUrl}/get-file`, formData, {
-            timeout: 600000,
             responseType: 'arraybuffer',
             headers: {
                 'Content-Type': 'multipart/form-data',
             }
-        })
+        });
         if (!response.data || !(response.data.length > 0)) {
             throw new Error('Empty or invalid file response from Flask API');
         }
-        const fileBuffer = Buffer.from(response.data)
-        res.set('Content-Type', 'application/octet-stream')
+        const fileBuffer = Buffer.from(response.data);
+
+        // Determine the content type based on the file extension
+        const contentType = response.headers['content-type'] || 'application/octet-stream';
+        res.set('Content-Type', contentType)
         res.send(fileBuffer)
 
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({ success: false, message: "Internal Server Error" })
+        console.log(error);
+        return res.status(500).json({ success: false, message: "Internal Server Error" });
     }
-})
+});
 
-export { router }
+export { router };
